41e8e1be558a380bd4c178db6bb986c8e5f390e3
leanglin2000 <leanglin2000@gmail.com>
2025-06-11 16:04:50 +0700

    This project is done

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,13 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', [HomeController::class,'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 //Route::get('/', function () {
- //   return view('index');  
+//   return view('index');  
 //})->name('home');
 
-Route::get('/article/{id}',[HomeController::class,'article'])->name('article');
+Route::get('/article/{id}', [HomeController::class, 'article'])->name('article');
 /*
 
 Route::get('/tag',function(){
@@ -33,34 +33,34 @@ Route::get('/tag',function(){
 });
 
 */
-Route::get('/login',[AuthController::class,'login'])->name('login');
-Route::post('/authenticate',[AuthController::class,'authenticate'])->name('authenticate');
-Route::post('/logout',[AuthController::class,'logout'])->name('logout');
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/authenticate', [AuthController::class, 'authenticate'])->name('authenticate');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::get('/tag',[DemoController::class,'tag']);
-Route::get('/category',[DemoController::class,'category']);
-Route::get('/blog',[DemoController::class,'blog']);
+Route::get('/tag', [DemoController::class, 'tag']);
+Route::get('/category', [DemoController::class, 'category']);
+Route::get('/blog', [DemoController::class, 'blog']);
 
 Route::prefix('admin')->name('admin.')->middleware('auth')->group(function () {
-    
-    Route::get('/category',[CategoryController::class,'index'])->name('category.index'); 
-    Route::get('/category/create',[CategoryController::class,'create'])->name('category.create'); 
-    Route::post('/category/store',[CategoryController::class,'store'])->name('category.store');
-    Route::get('/category/{id}',[CategoryController::class,'edit'])->name('category.edit'); 
-    Route::put('/category/{id}',[CategoryController::class,'update'])->name('category.update'); 
-    Route::delete('/category/{id}',[CategoryController::class,'destroy'])->name('category.destroy');
-    
-    Route::get('/tag',[TagController::class,'index'])->name('tag.index'); 
-    Route::get('/tag/create',[TagController::class,'create'])->name('tag.create'); 
-    Route::post('/tag/store',[TagController::class,'store'])->name('tag.store');
-    Route::get('/tag/{id}',[TagController::class,'edit'])->name('tag.edit'); 
-    Route::put('/tag/{id}',[TagController::class,'update'])->name('tag.update'); 
-    Route::delete('/tag/{id}',[TagController::class,'destroy'])->name('tag.destroy'); 
 
-    Route::get('/post',[postController::class,'index'])->name('post.index'); 
-    Route::get('/post/create',[postController::class,'create'])->name('post.create'); 
-    Route::post('/post/store',[postController::class,'store'])->name('post.store');
-    Route::get('/post/{id}',[postController::class,'edit'])->name('post.edit'); 
-    Route::put('/post/{id}',[postController::class,'update'])->name('post.update'); 
-    Route::delete('/post/{id}',[postController::class,'destroy'])->name('post.destroy'); 
+    Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
+    Route::get('/category/create', [CategoryController::class, 'create'])->name('category.create');
+    Route::post('/category/store', [CategoryController::class, 'store'])->name('category.store');
+    Route::get('/category/{id}', [CategoryController::class, 'edit'])->name('category.edit');
+    Route::put('/category/{id}', [CategoryController::class, 'update'])->name('category.update');
+    Route::delete('/category/{id}', [CategoryController::class, 'destroy'])->name('category.destroy');
+
+    Route::get('/tag', [TagController::class, 'index'])->name('tag.index');
+    Route::get('/tag/create', [TagController::class, 'create'])->name('tag.create');
+    Route::post('/tag/store', [TagController::class, 'store'])->name('tag.store');
+    Route::get('/tag/{id}', [TagController::class, 'edit'])->name('tag.edit');
+    Route::put('/tag/{id}', [TagController::class, 'update'])->name('tag.update');
+    Route::delete('/tag/{id}', [TagController::class, 'destroy'])->name('tag.destroy');
+
+    Route::get('/post', [postController::class, 'index'])->name('post.index');
+    Route::get('/post/create', [postController::class, 'create'])->name('post.create');
+    Route::post('/post/store', [postController::class, 'store'])->name('post.store');
+    Route::get('/post/{id}', [postController::class, 'edit'])->name('post.edit');
+    Route::put('/post/{id}', [postController::class, 'update'])->name('post.update');
+    Route::delete('/post/{id}', [postController::class, 'destroy'])->name('post.destroy');
 });
